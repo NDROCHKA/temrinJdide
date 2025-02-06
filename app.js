7eb994c6ae14f925f04ee217872fc2a37f6f851c
NDROCHKA <andrewkho006@gmail.com>
2025-02-06 19:14:32 +0200

    gaaee

---
+++ app.js
@@ -0,0 +1,7 @@
+import {plus , minus} from './plusminus.js';
+import { div, mult } from './multDiv';
+
+console.log("2+5 is:", plus(2,5));
+console.log("2-5 is:", minus(2,5));
+console.log("2*5 is:", mult(2,5));
+console.log("2/5 is:", div(2,5));
