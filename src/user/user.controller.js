3f56fdaa129b6614e25c7be5a16e36a80a7738aa
NDROCHKA <andrewkho006@gmail.com>
2025-02-25 23:54:30 +0200

    before charbel help

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,13 +1,11 @@
 import userService from "./user.service.js";
 
-const arrayAccounts = [];
-
 const signUp = async (req, res) => {
   try {
     const { firstname, lastname, password, email } = req.body;
     if(firstname != undefined && lastname != undefined && password !=undefined && email!=undefined){
     await userService.createAccount(firstname, lastname, password, email);
-    arrayAccounts.push([firstname,lastname,password,email]);
+    userService.arrayAccounts.push([firstname,lastname,password,email]);
     res.status(200).json({ message: "User added successfully" });}
     else { throw new Error("Missing required fields");}
   } catch (error) {
@@ -19,32 +17,13 @@ const signUp = async (req, res) => {
 const ChangeInfo = async(req,res) =>{
     try{
     const {InfoModifcationType , email , newInfo} = req.body;   
-    for (let i = 0; i < arrayAccounts.length; i++) {
-        // Check if the current account's email (4th element) matches the user input email
-        if (arrayAccounts[i][3] == email) {
-          switch (InfoModifcationType){
-         case 'firstName':
-            arrayAccounts[i][0] = newInfo;
-            break;
-         case 'lastName':
-            arrayAccounts[i][1] = newInfo;
-            break;
-         case'password':
-            arrayAccounts[i][2] = newInfo;
-            break;
-        }
-          } else {
-            throw new Error("Missing required fields");
-          }
-          // If you expect only one account per email, you can break out of the loop
-          break;
-        }
+
     await userService.InfoType(InfoModifcationType , email , newInfo) 
     res.status(200).json({message: "Info Changed Succesfully"})
     }
     catch(error){
         console.error("Error during changingInfo for user:", error);
-        res.status(500).json({ message: "Internal Server Error" });
+        res.status(500).json({ message: " wrong parameters or user email not found" });
     }
 }
 
--- src/user/user.service.js
+++ src/user/user.service.js
@@ -1,16 +1,42 @@
+let arrayAccounts = [];
+let counter = 0;
+let found = 0;
+
 const createAccount = async (firstname,lastname,password,email)=>{
 console.log("i am in my service file");
-console.log ("Data recived:\nfirstname:",firstname,"\nlastname:",lastname,"\npassword:",password,"\nemail",email     );
+console.log ("Data recived:\nfirstname:",firstname,"\nlastname:"
+            ,lastname,"\npassword:",password,"\nemail",email);
 }
 const InfoType = async (InfoModifcationType ,email , newInfo)=>{
 
+    while(counter < arrayAccounts.length){
+        // Check if the current account's email (4th element) matches the user input email
+        if (arrayAccounts[counter][3] == email) {
+            found++
+          switch (InfoModifcationType){
+         case 'firstName':
+            arrayAccounts[counter][0] = newInfo;
+            break;
+         case 'lastName':
+            arrayAccounts[counter][1] = newInfo;
+            break;
+         case'password':
+            arrayAccounts[counter][2] = newInfo;
+            break;
+        }
+          } 
+          counter++;
+        }
+        if (counter == arrayAccounts.length && found == 0 ){throw new Error("user email not found");}
+    
     console.log(`info of ${InfoModifcationType} is being modified in the email ${email}`)
-    console.log(`the new version is: ${newInfo}`)
+    console.log ("NEW INFO:\nfirstname:",arrayAccounts[counter][0],"\nlastname:",arrayAccounts[counter][1]
+                ,"\npassword:",arrayAccounts[counter][2],"\nemail",arrayAccounts[counter][3]);
 }
 
 
 
 
-export default {createAccount,InfoType};
+export default {createAccount,InfoType,arrayAccounts};
 
 
